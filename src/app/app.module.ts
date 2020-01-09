7f45140c062d9747a1be4ad7aa5bed0c90dfdc2e
Victor Sued <visued@gmail.com>
2020-01-09 20:49:45 -0300

    (usando pipes, parâmetros e pipes aninhados)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ExemplosPipesComponent } from './exemplos-pipes/exemplos-pipes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExemplosPipesComponent
   ],
   imports: [
     BrowserModule
